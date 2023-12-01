e2f1d275fa819e75059d6a5f052c10d3d58154c8
Mohamed Samir Salem <80456446+Mohamed-7018@users.noreply.github.com>
2023-12-01 09:23:30 +0200

    Update Scraper.java

--- Scrapper-Code/Scraper.java
+++ Scrapper-Code/Scraper.java
@@ -17,38 +17,6 @@ import java.util.List;
  * The Scraper class is responsible for scraping LeetCode problem data from the website.
  */
 public class Scraper {
-    private static final String USERNAME = ""; // Provide your LeetCode username
-    private static final String PASSWORD = ""; // Provide your LeetCode password
-    public static final int QUESTIONS_PAGE_WAIT_MILLIS = 25000;
-    public static final int LOGIN_PAGE_WAIT_MILLIS = 2000;
-    WebDriver driver;
-    List<String> companyURLs = new ArrayList<>();
-
-
-    
-Certainly! Here are the added JavaDoc comments for the provided code:
-
-java
-Copy code
-import com.opencsv.CSVWriter;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.openqa.selenium.*;
-import org.openqa.selenium.edge.EdgeDriver;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * The Scraper class is responsible for scraping LeetCode problem data from the website.
- */
-public class Scraper {
 
     private static final String USERNAME = ""; // Provide your LeetCode username
     private static final String PASSWORD = ""; // Provide your LeetCode password
@@ -75,7 +43,7 @@ public class Scraper {
         driver.findElement(By.xpath("// *[ @ id = 'id_login']")).sendKeys(USERNAME);
         driver.findElement(By.xpath("// *[ @ id = 'id_password']")).sendKeys(PASSWORD);
         driver.findElement(By.xpath("// *[ @ id = 'id_password']")).sendKeys(Keys.ENTER);
-        Thread.sleep(LOGIN_PAGE_WAIT_MILLIS); // Wait for the login to happen, then visit the problems pages
+        Thread.sleep(LOGIN_PAGE_WAIT_MILLIS);
         driver.get("https://leetcode.com/problemset/all/");
         List<WebElement> companies = driver.findElements(By.cssSelector(".mb-4.mr-3"));
         for (WebElement company : companies) {
@@ -88,8 +56,6 @@ public class Scraper {
         }
     }
 
-
-    
     /**
      * Visits a specific company's LeetCode page and extracts problem data.
      *
@@ -101,7 +67,7 @@ public class Scraper {
         String companyName = companyURL.substring(companyURL.lastIndexOf("/") + 1);
         System.out.println("Visiting " + companyURL);
         driver.get(companyURL);
-        Thread.sleep(QUESTIONS_PAGE_WAIT_MILLIS); // Wait for the page to load, for companies like Google/Amazon, it takes a lot of time
+        Thread.sleep(QUESTIONS_PAGE_WAIT_MILLIS);
         String table = "";
         try {
             table = "<table>" + driver.findElement(By.className("table")).getAttribute("innerHTML") + "</table>";
@@ -111,14 +77,14 @@ public class Scraper {
             Thread.sleep(30000);
             table = "<table>" + driver.findElement(By.className("table")).getAttribute("innerHTML") + "</table>";
         }
-        Document doc = Jsoup.parse(table); // parse the table html content
+        Document doc = Jsoup.parse(table);
         List<String[]> result = new ArrayList<>();
         String[] header = new String[]{"ID", "Title", "URL", "Is Premium", "Acceptance %", "Difficulty", "Frequency %"};
         result.add(header);
         for (Element row : doc.getElementsByTag("tr")) {
             Elements cols = row.getElementsByTag("td");
             int size = cols.size();
-            if (size != 0) { // for <th> size would be 0
+            if (size != 0) {
                 String id = cols.get(1).text();
                 String title = cols.get(2).text();
                 Elements href = cols.get(2).getElementsByAttribute("href");
@@ -127,8 +93,7 @@ public class Scraper {
                 String acceptance = cols.get(3).text();
                 String difficulty = cols.get(4).getElementsByTag("span").text();
                 String frequency = cols.get(5).getElementsByClass("progress-bar").attr("style");
-                // sample response "width: 29.1345%";
-                frequency = frequency.substring(frequency.indexOf(" ") + 1); // get the value after the first whitespace
+                frequency = frequency.substring(frequency.indexOf(" ") + 1);
                 String[] res = new String[]{id, title, problemUrl, isPremium ? "Y" : "N", acceptance, difficulty, frequency};
                 result.add(res);
             }
